13ba6471e556b59d6eacfa84502ff8a19ee2c56b
StevenTinoco <StevenTinoco05@gmail.com>
2026-05-07 14:16:06 -0700

    Implement core user persistence system

---
+++ api/src/main/java/com/canigetafiver/lifemyway/api/Expense.java
@@ -0,0 +1,50 @@
+package com.canigetafiver.lifemyway.api;
+
+/**
+ * Represents one financial transaction.
+ */
+public class Expense {
+    private String description;
+    private double amount;
+    private String date;
+    private Category category;
+
+    public Expense(String description, double amount, String date, Category category) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
